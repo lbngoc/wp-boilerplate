a1cd7e5cfabbf92163c72fb9410cb5a25af19749
Ngoc Luong <baongock32@gmail.com>
2019-05-13 14:22:03 +0700

    Split main JS file to multiple components

---
+++ src/assets/scripts/_home.js
@@ -0,0 +1,10 @@
+const HomePage = {
+  init: () => {
+    console.log('HomePage is initialized.');
+  },
+  finalize: () => {
+    console.log('Finally Event (HomePage) is called.');
+  }
+};
+
+export default HomePage;
